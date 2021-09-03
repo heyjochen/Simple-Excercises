dc8d401ad3aecaeba4ad19dd0dfad3e278d70ce3
heyjochen <jochen@stierberger.com>
2021-09-03 08:37:38 -0500

    count all odd numbers below n

---
+++ 8kyu/countTheOdds.js
@@ -0,0 +1,18 @@
+/*
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+*/
+let odds = [];
+for (let i = 1; i < n; i = i + 2) {
+  odds.push[i];
+}
+return odds;
+
+//Or easy
+
+return Math.floor(n / 2);
