d44dc8037aa33e2d787c24bc2690814da9322711
heyjochen <jochen@stierberger.com>
2021-12-03 06:58:48 -0600

    test if array of objects contains value

---
+++ 7kyu/codingMeetup3.js
@@ -0,0 +1,59 @@
+/*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+your function should return true.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+
+
+*/
+
+//P: array of objects
+//R: true if Ruby in there
+//E: see above
+//P: extract the objects into an array, we do Object.keys(list1).map( e => e.language).filter(e => e==='Ruby), length >= 1
+let list = [
+  {
+    firstName: "Emma",
+    lastName: "Z.",
+    country: "Netherlands",
+    continent: "Europe",
+    age: 29,
+    language: "Ruby",
+  },
+  {
+    firstName: "Piotr",
+    lastName: "B.",
+    country: "Poland",
+    continent: "Europe",
+    age: 128,
+    language: "Javascript",
+  },
+  {
+    firstName: "Jayden",
+    lastName: "P.",
+    country: "Jamaica",
+    continent: "Americas",
+    age: 42,
+    language: "JavaScript",
+  },
+];
+
+function isRubyComing(list) {
+  return list.findIndex((dev) => dev.language === "Ruby") != -1;
+}
+
+isRubyComing(list);
