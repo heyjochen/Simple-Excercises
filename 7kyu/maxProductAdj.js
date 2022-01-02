8669b59326a73641508713432bcd0ce8afab6856
heyjochen <jochen@stierberger.com>
2022-01-02 06:54:11 -0600

    calculate the max product possible in an array

---
+++ 7kyu/maxProductAdj.js
@@ -0,0 +1,19 @@
+/*
+
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+Input >> Output Examples
+adjacentElementsProduct([1, 2, 3]); ==> return 6
+Explanation:
+The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+
+*/
+
+function adjacentElementsProduct(array) {
+  // max product
+}
