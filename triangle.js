1991a04ae455798b98893077bcc92fe8091c2e56
heyjochen <94847959+heyjochen@users.noreply.github.com>
2022-03-11 12:52:19 -0600

    Completed triangle and added chessboard

---
+++ triangle.js
@@ -0,0 +1,18 @@
+/* 
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+*/
+
+let octothorpe = ''
+for (let i = 0; i < 7; i++) {
+octothorpe += '#'
+console.log(octothorpe)
+}
