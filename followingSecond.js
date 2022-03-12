72a5b0f7c9b480a9917b921152e280ba1de608dd
heyjochen <94847959+heyjochen@users.noreply.github.com>
2022-03-12 09:11:40 -0600

    wrote pseudo code

--- followingSecond.js
+++ followingSecond.js
@@ -6,3 +6,43 @@ This is not as simple as it seems... Look at the following results to see for yo
 14h17m59s => 14h18m0s
 6h59m59s => 7h0m0s
 23h59m59s => 0h0m0s (midnight) */
+
+
+/*
+create variable and prompt for a an hour
+create variable and prompt for a minute
+create variable and prompt for a second
+
+create if conditions
+
+alert final time 
+
+
+// Code // 
+
+let hour = prompt('Give me an hour')
+let minute = prompt('Give me a minute')
+let second = prompt('Give me an second')
+
+if hour === 23 && minute === 59 && second === 59
+hour = 0 
+minute = 0
+second = 0
+
+if hour < 23 && minute < 59 && second < 59 
+second += 1
+
+if hour < 23 && minute < 59 && second === 59
+minute += 1
+second = 0
+
+if hour < 23 && minute === 59 && second === 59
+hour += 1
+minute = 0
+second = 0
+
+if hour > 23 || minute > 59 || second > 59
+alert('This is not a correct time')
+
+
+alert(`Your time one second after your input is ${hour}h${minute}m${second}s`)
