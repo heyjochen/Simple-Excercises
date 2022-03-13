04d83711c6ca17e80c0fab516ae05eef6ae47ae8
heyjochen <94847959+heyjochen@users.noreply.github.com>
2022-03-13 10:17:31 -0500

    did checkRange

---
+++ checkRange.js
@@ -0,0 +1,26 @@
+/*Check the range between
+importance: 3
+Write an if condition to check that age is between 14 and 90 inclusively.
+
+“Inclusively” means that age can reach the edges 14 or 90.
+*/
+
+if (age >= 14 && age <= 90) {
+
+}
+
+/*
+Check the range outside
+importance: 3
+Write an if condition to check that age is NOT between 14 and 90 inclusively.
+
+Create two variants: the first one using NOT !, the second one – without it.
+*/
+
+if (age < 14 || age > 90) {
+
+}
+
+if (!(age >= 14 && age <= 90)) {
+
+}
--- followingSecond.js
+++ followingSecond.js
@@ -1,28 +1,30 @@
-/* Following second
+/* 
 Write a program that asks for a time under the form of three information (hours, minutes, seconds). The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
 
 This is not as simple as it seems... Look at the following results to see for yourself:
 
 14h17m59s => 14h18m0s
 6h59m59s => 7h0m0s
-23h59m59s => 0h0m0s (midnight) */
+23h59m59s => 0h0m0s (midnight) 
 
 
-/*
-create variable and prompt for a an hour
-create variable and prompt for a minute
-create variable and prompt for a second
+// Pseudo Code // 
+create const and prompt for time
+create variable and extract the substrings for hour, minute and second
 
 create if conditions
 
 alert final time 
 
 
-// Code // 
 
 let hour = prompt('Give me an hour')
 let minute = prompt('Give me a minute')
 let second = prompt('Give me an second')
+
+if hour > 23 || minute > 59 || second > 59
+alert('This is not a correct time')
+
 
 if hour === 23 && minute === 59 && second === 59
 hour = 0 
@@ -41,8 +43,49 @@ hour += 1
 minute = 0
 second = 0
 
-if hour > 23 || minute > 59 || second > 59
-alert('This is not a correct time')
 
 
 alert(`Your time one second after your input is ${hour}h${minute}m${second}s`)
+
+*/
+//Create constant timeFormat like 14h17m59s
+const timeFormat = prompt('Give me a time in the following format 14h17m59s.\nDon\t forget the h after hours, the m after minutes and the s after seconds')
+
+//Extract variables out of input
+let hours = Number(timeFormat.substring(0, timeFormat.indexOf("h")))
+let minutes = Number(timeFormat.substring(timeFormat.indexOf("h") + 1, timeFormat.indexOf("m")))
+let seconds = Number(timeFormat.substring(timeFormat.indexOf("m") + 1, timeFormat.indexOf("s")))
+
+//Create function
+function updateTimeFormat (){
+
+    if (hours === 23 && minutes === 59 && seconds === 59) {
+        hours = 0 
+        minutes = 0
+        seconds = 0
+    }
+
+    else if (hours <= 23 && minutes <= 59 && seconds < 59) {
+        seconds += 1
+    }
+
+    else if (hours <= 23 && minutes < 59 && seconds === 59) {
+        minutes += 1
+        seconds = 0        
+    }
+
+    else if (hours <= 23 && minutes === 59 && seconds === 59) {
+        hours += 1
+        minutes = 0
+        seconds = 0
+    }
+
+    else {
+        alert('This is not the correct input')
+    }
+
+    console.log(`${hours}h${minutes}m${seconds}s`)    
+}
+
+updateTimeFormat()
+
