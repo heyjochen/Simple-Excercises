04dc951494f2737f43ecb4339519d3c3a3d62b20
heyjochen <94847959+heyjochen@users.noreply.github.com>
2022-03-13 10:38:25 -0500

    Practicing switch statements

---
+++ trySwitchStatement.js
@@ -0,0 +1,13 @@
+// Practice some switch statements
+const beer = prompt('What beer do you like?')
+switch (beer) {
+    case 'Miller':
+        console.log('High Life')
+        break;
+    case 'Lagunitas':
+        console.log('Top Choice')
+        break;
+    case 'Budweiser':
+        alert('Are you sure about that?')
+        break;
+}
