9148e8bfc9704fae3bbdb10ce70c42366dcfc54d
heyjochen <jochen@stierberger.com>
2022-03-21 17:00:57 -0500

    created but not finished, wrote pseudo

--- chessboard.js
+++ chessboard.js
@@ -11,3 +11,18 @@ Passing this string to console.log should show something like this:
  # # # #
 # # # #
 When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height. */
+
+// 8 rows and 8 colums
+// first row starts with empty space
+// second row starts with octathorpe
+// third row starts with empty space
+// first row for loop 0 to 7 starting with empty and then adding octathorpe
+// second row for loop 0 to 7 starting with octathorpe and then adding empty space
+// that means we need a loop inside a loop
+// create a loop for the row that has a loop for the column
+
+let rows = ''
+let columns = ''
+for (let i = 0; i < 7; i++) {
+    
+}
