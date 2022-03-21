e70ee557bc0073fd0dab89a70616565f852dd39f
heyjochen <jochen@stierberger.com>
2022-03-21 13:00:56 -0500

    solved simple excercise

---
+++ pyramid.js
@@ -0,0 +1,13 @@
+/* 
+Create a pyramid with a loop and up to 8 rows
+*/
+
+// create a function with a loop up to 8 
+// every iteration it prints one more octathorpe
+
+
+let triangle = "#"
+for (let i = 0; i < 7; i++) {
+  console.log(triangle)
+  triangle += "#"
+}
