fac8cde09789c20b29920b0a5371b94f28014cef
heyjochen <jochen@stierberger.com>
2022-03-22 16:24:18 -0500

    finished challange

---
+++ maptoObjects.js
@@ -0,0 +1,21 @@
+/*
+Map to objects
+importance: 5
+You have an array of user objects, each one has name, surname and id.
+
+Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+For instance:
+*/
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(item => ({
+    fullName: `${item.name} ${item.surname}`, 
+    id: item.id}))
+
+console.log(usersMapped)
