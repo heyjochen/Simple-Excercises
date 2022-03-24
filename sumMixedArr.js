3f504043564b33a52a529f2d28b48a5cd95691a7
heyjochen <jochen@stierberger.com>
2022-03-24 14:49:03 -0500

    finished challenge

--- sumMixedArr.js
+++ sumMixedArr.js
@@ -4,7 +4,6 @@ Given an array of integers as strings and numbers, return the sum of the array v
 
 */
 
-let arr = ['5', '0', 9, 3, 2, 1, '9', 6, 7]
-
-arr.reduce(a,b => a+b)
-console.log(arr)
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+  }
