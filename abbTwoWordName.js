3a502b79d9ba99d9ca07e0a41c559f08dc0c1d59
heyjochen <jochen@stierberger.com>
2022-03-29 11:49:39 -0500

    finish challenge

---
+++ abbTwoWordName.js
@@ -0,0 +1,30 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+*/
+
+//Split the String into an array
+//Map through every element and extract the first Character
+//toUpperCase
+//join
+function abbrevName(name){
+    return name.split(' ').map(x => x.substr(0, 1).toUpperCase()).join('.');
+  }
+
+
+//Split
+//access first Character through nested bracket
+//toUpperCase
+function abbrevName(name){
+
+    let nameArray = name.split(' ');
+    return (nameArray[0][0] + '.' + nameArray[1][0]).toUpperCase();
+  }
---
+++ removeSpaces.js
@@ -0,0 +1,9 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+    // using split with delim space
+    // using join with empty delim
+    return x.split(' ').join('')
+}
