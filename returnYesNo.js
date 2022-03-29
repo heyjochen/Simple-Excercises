945ae327ed6ae7f6a90e129c2882a8fc0ba640e3
heyjochen <jochen@stierberger.com>
2022-03-29 11:11:09 -0500

    finish challenge

---
+++ returnYesNo.js
@@ -0,0 +1,10 @@
+/*
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+// ternary operator if true === true then yes else no
+
+function boolToWord( bool ){
+    return (bool) ? 'Yes' : 'No'
+   }
