b67ac1c725f26860d131f03eb7a78eeab2a2f93f
heyjochen <jochen@stierberger.com>
2022-03-30 15:00:44 -0500

    finish challenge

---
+++ countMonkeys.js
@@ -0,0 +1,29 @@
+/*
+
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example:
+
+monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+monkeyCount(1) // --> [1]
+
+ */
+
+
+// Simple for loop
+
+function monkeyCount (n) {
+    let emptyArr = []
+    for (let i=1; i<=n; i++){
+        emptyArr.push(i)
+    }
+    return emptyArr
+}
+
+// making a new Array
+
+function monkeyCount(n) {
+    return Array.from({length:n}, (_,i)=>i+1)
+  }
