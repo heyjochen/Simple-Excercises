34be30e57f01916637ef196d37c9fa65525b1561
heyjochen <jochen@stierberger.com>
2022-04-02 14:53:53 -0500

    finish challenge

--- findPrime.js
+++ findPrime.js
@@ -18,11 +18,10 @@ P.S. The code should work for any n, not be hard-tuned for any fixed value.
 let n = 10;
 
 nextPrime:
-for (let i = 2; i <= n; i++) { // for each i...
-
-  for (let j = 2; j < i; j++) { // look for a divisor..
-    if (i % j == 0) continue nextPrime; // not a prime, go next i
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue nextPrime;
   }
 
-  alert( i ); // a prime
+  alert( i );
 }
