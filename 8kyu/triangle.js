8881062ae6ed95f41d78a9e475d9bea3c3300c80
heyjochen <jochen@stierberger.com>
2022-04-05 16:08:36 -0500

    update challenge solution

--- 8kyu/triangle.js
+++ 8kyu/triangle.js
@@ -11,6 +11,36 @@ Write a loop that makes seven calls to console.log to output the following trian
 It may be useful to know that you can find the length of a string by writing .length after it.
 */
 
+//P: number of rows
+//R: triangle
+//E: see above
+//P: create a variable for octathorpe, create while loop, add octathorpe to triangle, introduce new line
+
+
+let octathorpe = ''
+function makeTriangle(num) {
+    while (octathorpe.length <= num) {
+        console.log(octathorpe)
+        octathorpe += '#'    
+    }
+
+}
+makeTriangle(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let octothorpe = ''
 for (let i = 0; i < 7; i++) {
 octothorpe += '#'
