bdc1d439e51b3b6cb84ec2042ac16efcc918e79e
heyjochen <jochen@stierberger.com>
2022-04-26 14:34:46 -0500

    finish challenge

---
+++ 7kyu/removeAnchor.js
@@ -0,0 +1,20 @@
+/*
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+*/
+
+function removeUrlAnchor(url){
+    return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
+  }
+
+// Using split we can write shorter code
+
+function removeUrlAnchor(url){
+    return url.split('#')[0];
+}
+
