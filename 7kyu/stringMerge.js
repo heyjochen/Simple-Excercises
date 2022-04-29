09e0e27b26f9b14cf09dddcf637685912eb1f271
heyjochen <jochen@stierberger.com>
2022-04-29 14:35:37 -0500

    finish challenge

--- 7kyu/stringMerge.js
+++ 7kyu/stringMerge.js
@@ -10,6 +10,7 @@ Examples
 function stringMerge(string1, string2, letter){
     const index1 = string1.indexOf(letter)
     const index2 = string2.indexOf(letter)
-    
     return string1.slice(0, index1)+string2.slice(index2, string2.length)
   }
+
+stringMerge = (a, b, l) => a.slice(0, a.indexOf(l)) + b.slice(b.indexOf(l));
