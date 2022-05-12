4e5996f1fa603d6c602fb110b336aee05296d6c4
heyjochen <jochen@stierberger.com>
2022-05-12 12:59:05 -0500

    write function to remove duplicate words using new Set

---
+++ 7kyu/removeDuplicates.js
@@ -0,0 +1,16 @@
+/*
+
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+*/
+
+const removeDuplicateWords = string => [...new Set(s.split(' '))].join(' ')
