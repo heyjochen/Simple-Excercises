13caff2d328a48f90b5c38e72efccfba821126ee
heyjochen <jochen@stierberger.com>
2022-05-15 11:12:38 -0500

    find number of digits used in k**2

---
+++ 7kyu/countOneDigit.js
@@ -0,0 +1,40 @@
+/*
+
+Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+Examples:
+n = 10, d = 1 
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+nb_dig(25, 1) returns 11 since
+the k*k that contain the digit 1 are:
+1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+So there are 11 digits 1 for the squares of numbers between 0 and 25.
+Note that 121 has twice the digit 1.
+
+*/
+
+//P: integer n and integer d
+//R: number of digits d used in k**2
+//E:  nb_dig(25, 1) returns 11 since 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+//P: make a new array that pushes k*k between 0 and n, str.includes(d) count++
+
+function nbDig(n, d) {
+
+    let holder = []
+    let count = 0 
+    for (let i=0; i<=n; i++) {
+        holder.push(i*i)
+    }
+
+    // make string out of array and then ssplit filter
+    count = holder.join('').split('').filter(e  => e === 1).length
+    return count
+}
