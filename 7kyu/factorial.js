c94d10498921cbaa667ebbe44a5b068e16eec8bb
heyjochen <jochen@stierberger.com>
2022-06-07 08:16:30 -0500

    finish calculating factorial function

---
+++ 7kyu/factorial.js
@@ -0,0 +1,20 @@
+/*
+
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+
+*/
+
+function factorial(n) {
+  let answer = 1;
+
+  while (n > 0) {
+    answer *= n;
+    n--;
+  }
+
+  return answer;
+}
+
+factorial(5);
