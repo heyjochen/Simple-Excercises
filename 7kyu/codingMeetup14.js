5f8c3b8c3f47b9f2cd0ad13d25b79e11fd17361a
heyjochen <jochen@stierberger.com>
2022-06-17 07:34:21 -0500

    reduce an arr of objects

---
+++ 7kyu/codingMeetup14.js
@@ -0,0 +1,35 @@
+/*
+
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form..
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+    meal: 'vegan' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+    meal: 'vegetarian' },
+];
+your function should return the following object (the order of properties does not matter):
+
+{ vegetarian: 2, standard: 1, vegan: 1 }
+
+*/
+//P: arr of objects
+//R: object food options count
+//E:
+//P: reduce through like previous
+
+function orderFood(list) {
+  return list.reduce((pot, curr) => {
+    if (curr.meal in pot) pot[curr.meal]++;
+    else pot[curr.meal] = 1;
+    return pot;
+  }, {});
+}
