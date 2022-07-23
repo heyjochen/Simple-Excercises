15e039cbb7e449d388b519475db4ce2ad5899400
heyjochen <jochen@stierberger.com>
2022-07-22 19:50:15 -0500

    change variable 0 to 1

--- 8kyu/chainingLadder.js
+++ 8kyu/chainingLadder.js
@@ -25,18 +25,18 @@ Modify the code of up, down and showStep to make the calls chainable, like this:
 ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
 */
 let ladder = {
-    step: 0,
-    up() {
-      this.step++;
-      return this
-    },
-    down() {
-      this.step--;
-      return this
-    },
-    showStep() {
-      console.log( this.step );
-      return this
-    }
-}
+  step: 1,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    console.log(this.step);
+    return this;
+  },
+};
 ladder.up().up().down().showStep().down().showStep();
