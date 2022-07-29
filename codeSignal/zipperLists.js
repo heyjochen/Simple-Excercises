1ba4e3f2a9a07769598e475f27328a8f3c93b2ee
heyjochen <jochen@stierberger.com>
2022-07-29 10:02:29 -0500

    zip two linked lists together

---
+++ codeSignal/zipperLists.js
@@ -0,0 +1,89 @@
+/*
+
+zipper lists
+
+Write a function, zipperLists, that takes in the head of two linked lists as arguments. The function should zipper the two lists together into single linked list by alternating nodes. If one of the linked lists is longer than the other, the resulting list should terminate with the remaining nodes. The function should return the head of the zippered linked list.
+
+Do this in-place, by mutating the original Nodes.
+
+You may assume that both input lists are non-empty.
+test_00:
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+a.next = b;
+b.next = c;
+// a -> b -> c
+
+const x = new Node("x");
+const y = new Node("y");
+const z = new Node("z");
+x.next = y;
+y.next = z;
+// x -> y -> z
+
+zipperLists(a, x);
+// a -> x -> b -> y -> c -> z
+
+test_01:
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+// a -> b -> c -> d -> e -> f
+
+const x = new Node("x");
+const y = new Node("y");
+const z = new Node("z");
+x.next = y;
+y.next = z;
+// x -> y -> z
+
+zipperLists(a, x);
+// a -> x -> b -> y -> c -> z -> d -> e -> f
+
+*/
+
+// Recursive
+const zipperLists = (head1, head2) => {
+  if (head1 === null && head2 === null) return null;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
+  const next1 = head1.next;
+  const next2 = head2.next;
+  head1.next = head2;
+  head2.next = zipperLists(next1, next2);
+  return head1;
+};
+
+// Iterative
+const zipperLists2 = (head1, head2) => {
+  let tail = head1;
+  let current1 = head1.next;
+  let current2 = head2;
+  let count = 0;
+  while (current1 && current2) {
+    if (count % 2) {
+      tail.next = current1;
+      current1 = current1.next;
+    } else {
+      tail.next = current2;
+      current2 = current2.next;
+    }
+    tail = tail.next;
+    count++;
+  }
+  if (current1) tail.next = current1;
+  if (current2) tail.next = current2;
+
+  return head1;
+};
