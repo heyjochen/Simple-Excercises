1ef7b6fa3c7db9f90e3eb02e2200f669e8d81779
heyjochen <jochen@stierberger.com>
2022-08-30 09:39:23 -0500

    create hashmap to solve problem

---
+++ leetcode/jewelsNStones771.js
@@ -0,0 +1,39 @@
+/*
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+*/
+
+var numJewelsInStones = function (jewels, stones) {
+  // create a hashmap that stores a value of true for every jewel
+  // loop through every stone and if it is in hashmap increase the count
+  let count = 0;
+  let hashmap = {};
+
+  for (let jewel of jewels) {
+    hashmap[jewel] = true;
+  }
+
+  for (let stone of stones) {
+    if (hashmap[stone]) {
+      count++;
+    }
+  }
+
+  return count;
+};
