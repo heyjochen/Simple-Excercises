450fb0792c43d363ca591212638a2b8194bf4cdc
Jochen Stierberger <jochen@stierberger.com>
2022-09-29 09:42:23 -0500

    hashmap to solve array challenge

---
+++ leetcode/137singleNumberII.js
@@ -0,0 +1,21 @@
+/*
+
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+*/
+
+var singleNumber = function (nums) {
+  const hash = {};
+  for (i in nums) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else if (hash[nums[i]] && hash[nums[i]] > 1) {
+      delete hash[nums[i]];
+    } else {
+      hash[nums[i]]++;
+    }
+  }
+  return Object.keys(hash)[0];
+};
