6ee04618ce3078ac238318af99b5bf2d1dc98502
Jochen Stierberger <jochen@stierberger.com>
2022-10-05 09:39:21 -0500

    dynamic programming

---
+++ leetcode/118PascalsTriangle.js
@@ -0,0 +1,28 @@
+/*
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+
+*/
+
+var generate = function (numRows) {
+  var pascal = [];
+  for (var i = 0; i < numRows; i++) {
+    pascal[i] = [];
+    pascal[i][0] = 1;
+    for (var j = 1; j < i; j++) {
+      pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+    }
+    pascal[i][i] = 1;
+  }
+  return pascal;
+};
