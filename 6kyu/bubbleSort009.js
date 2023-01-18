9ccb2ec527d890b4d4d7bb7150e07b489e24bc33
Jochen Stierberger <jochen@stierberger.com>
2023-01-18 06:40:12 -0600

    implement bubble Sort algo

---
+++ 6kyu/bubbleSort009.js
@@ -0,0 +1,16 @@
+let arra = [5, 2, 1, 6, 7];
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = 0; j < arr.length - i; ++j) {
+      if (arr[j + 1] < arr[j]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort(arra));
