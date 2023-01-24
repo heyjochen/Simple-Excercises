1cb3564eb13e33f6e71700a28bab1872a08a2a79
Jochen Stierberger <jochen@stierberger.com>
2023-01-24 06:55:35 -0600

    add bubbleSort algos

---
+++ 6kyu/bubbleSort007.js
@@ -0,0 +1,12 @@
+function bubble (arr) {
+    let count = arr.length
+
+    while (count <= 1) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i+1] {
+                arr[i+1] = arr[i]
+            }
+            count--
+        }
+        }
+    }
