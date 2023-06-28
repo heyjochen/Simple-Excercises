a96b50608c0758ad47c152688ac76fd97b39815c
Jochen Stierberger <jochen@stierberger.com>
2023-06-28 06:26:25 -0500

    This is the better solution

--- leetcode/75/Arrays_and_Hashing/group_anagrams.js
+++ leetcode/75/Arrays_and_Hashing/group_anagrams.js
@@ -1,9 +1,24 @@
 const groupAnagrams = strs => {
-  // BF solution
+  o  // let hash = {}
+  // for (let str of strs) {
+  //   let sorted = str.split("").sort().join("")
+  //   hash[sorted] ? hash[sorted].push(str) : hash[sorted] = [str]
+  // }
+  //
+  // return Object.values(hash)
+
+  // Optimized
   let hash = {}
+
   for (let str of strs) {
-    let sorted = str.split("").sort().join("")
-    hash[sorted] ? hash[sorted].push(str) : hash[sorted] = [str]
+    let arr = Array(26).fill(0)
+
+    for (let char of str) {
+      arr[char.charCodeAt() - 97]++
+    }
+
+    let count = arr.join('.')
+    hash[count] ? hash[count].push(str) : hash[count] = [str]
   }
 
   return Object.values(hash)
